85f3729154a99a5bd453e5d3bbad3e781a2e9266
Sven McCall Jr <sven@fatmap.com>
2021-02-06 17:18:15 +0100

    [SB-5] Add Oregon scaper
    - This commit actually removes the washington scraper as I noticed that some forecast offices use a generic NAC API setup. Because of this, we can setup a simple generic scrpaer with a method that takes these parameters:
      - `center_id`: The string character representing the resporting office (Ex: NWAC)
      - `zone_id_list`: The array that holds which IDs need to be on the end of the URLs to get all the data (this is something that needs to be looked up)
    - Also wrote a generic method in `main.rb` which writes the data to the correct location. This method takes the params:
      - `directory`: What state folder it belongs in
      - `center_id`: The NAC center ID
      - `data`: The hash holding the data we collected for today

---
+++ scrapers/US/generic_NAC.rb
@@ -0,0 +1,32 @@
+require 'httparty'
+require 'json'
+
+class GenericNAC
+  BASE_URL = 'https://api.avalanche.org/v2/public/product?type=forecast&center_id='
+
+  def self.data_collector(center_id:, zone_id_list:)
+    # Data Collector takes a NAC center ID and zone ID
+    # and returns the JSON from the API for that data
+    data = {}
+    zone_id_list.each do |id|
+      url = url_constructor(center_id, id)
+
+      # We need to rescue because some of the links
+      # have broken data
+      begin
+        request = HTTParty.get(url)
+        json_format = JSON(request.body)
+      rescue
+        p "URL returned bad data"
+        data[id] = {NOTE: 'BAD DATA'}
+      else
+        data[id] = JSON(request.body)
+      end
+    end
+    data
+  end
+
+  def self.url_constructor(center_id, zone_id)
+    BASE_URL + center_id.to_s + '&zone_id=' + zone_id.to_s
+  end
+end
--- scrapers/main.rb
+++ scrapers/main.rb
@@ -1,3 +1,37 @@
 # Main file to simply trigger other scrapers to pull their data
 
-require_relative 'US/washington/nac_washington'
+#require_relative 'US/washington/nac_washington'
+require 'json'
+require_relative 'US/generic_NAC.rb'
+
+# Support functions
+def write_data(directory:, center_id:, data:)
+  # Make the subfolder if it doesn't exist
+  Dir.mkdir(directory) unless Dir.exists?(directory)
+  # Write the data to a file
+  filename = directory + '/' + Date.today().to_s + "_#{center_id.to_s}.json"
+  File.open(filename,"w") do |f|
+    f.write(JSON.pretty_generate(data))
+  end
+end
+
+# For washington
+id_list = Array (139..148)
+data = GenericNAC.data_collector(center_id: 'NWAC', zone_id_list: id_list)
+write_data(directory: 'data/washington', center_id: 'NWAC', data: data)
+
+# OREGON
+# => COAA
+id_list = [205, 300]
+data = GenericNAC.data_collector(
+  center_id: 'COAA',
+  zone_id_list: id_list
+)
+write_data(directory: 'data/oregon', center_id: 'COAA', data: data)
+
+# => WAC
+data = GenericNAC.data_collector(
+  center_id: 'WAC',
+  zone_id_list: [276]
+)
+write_data(directory: 'data/oregon', center_id: 'WAC', data: data)
