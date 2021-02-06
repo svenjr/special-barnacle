6bb823158a7eba0b84b57fdbb367b84211cf2585
Sven McCall Jr <sven@fatmap.com>
2021-02-06 18:36:11 +0100

    [SB-6] Add Idaho generic catching

--- scrapers/main.rb
+++ scrapers/main.rb
@@ -15,12 +15,12 @@ def write_data(directory:, center_id:, data:)
   end
 end
 
-# For washington
+# For washington (one subset)
 id_list = Array (139..148)
 data = GenericNAC.data_collector(center_id: 'NWAC', zone_id_list: id_list)
 write_data(directory: 'data/washington', center_id: 'NWAC', data: data)
 
-# OREGON
+# OREGON (2 subsets)
 # => COAA
 id_list = [205, 300]
 data = GenericNAC.data_collector(
@@ -35,3 +35,40 @@ data = GenericNAC.data_collector(
   zone_id_list: [276]
 )
 write_data(directory: 'data/oregon', center_id: 'WAC', data: data)
+
+# IDAHO (3 subsets)
+# => Sawtooths
+id_list = [293,294,295,296]
+data = GenericNAC.data_collector(
+  center_id: 'SNFAC',
+  zone_id_list: id_list
+)
+write_data(
+  directory: 'data/idaho',
+  center_id: 'SNFAC',
+  data: data
+)
+
+# => Panhandle
+id_list = [138,149,272]
+data = GenericNAC.data_collector(
+  center_id: 'IPAC',
+  zone_id_list: id_list
+)
+write_data(
+  directory: 'data/idaho',
+  center_id: 'IPAC',
+  data: data
+)
+
+# => Payette (PAC)
+id_list = [153]
+data = GenericNAC.data_collector(
+  center_id: 'PAC',
+  zone_id_list: id_list
+)
+write_data(
+  directory: 'data/idaho',
+  center_id: 'PAC',
+  data: data
+)
